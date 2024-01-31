c503d1c7cb195093f6b022a5b4e73c49eb7304ea
naveendanj <naveenhettiwaththa@gmail.com>
2024-01-31 12:53:23 +0530

    added get all product endpoint

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -6,108 +6,200 @@ import Product from '../models/product.model';
 import fs from 'fs'
 
 router.post(
-    '/create-product',
-    upload.fields([{ name: 'images', maxCount: 8 }]),
-    async (req: Request, res: Response, next: NextFunction) => {
+  '/create-product',
+  upload.fields([{ name: 'images', maxCount: 8 }]),
+  async (req: Request, res: Response, next: NextFunction) => {
 
-      const validator = Joi.object({
-        sku: Joi.string().required(),
-        productName: Joi.string().required(),
-        description: Joi.string().required(),
-        quantity: Joi.number().required(),
-        thumbnail: Joi.number().required(),
+    const validator = Joi.object({
+      sku: Joi.string().required(),
+      productName: Joi.string().required(),
+      description: Joi.string().required(),
+      quantity: Joi.number().required(),
+      thumbnail: Joi.number().required(),
+    });
+
+    try {
+      const data = await validator.validateAsync(req.body, { abortEarly: false });
+
+      const productSearch = await Product.findOne({ sku: data.sku });
+
+      if (productSearch) {
+        // @ts-ignore
+        upload_rollback(req.files['images'] as Express.Multer.File[])
+
+        return res.status(400).json({
+          message: 'Product SKU is already assigned to another product!',
+        });
+      }
+
+      if (data.quantity < 0) {
+
+        // @ts-ignore
+        upload_rollback(req.files['images'] as Express.Multer.File[])
+
+        return res.status(400).json({
+          message: 'Invalid quantity!',
+        });
+      }
+
+      next();
+    } catch (err) {
+
+      // @ts-ignore
+      upload_rollback(req.files['images'] as Express.Multer.File[])
+
+      return res.status(400).json({
+        message: 'Error while creating product',
+        error: err,
       });
-  
-      try {
-        const data = await validator.validateAsync(req.body, { abortEarly: false });
-  
-        const productSearch = await Product.findOne({ sku: data.sku });
-  
-        if (productSearch) {
-            // @ts-ignore
-            upload_rollback(req.files['images'] as Express.Multer.File[])
-
-            return res.status(400).json({
-                message: 'Product SKU is already assigned to another product!',
-            });
-        }
-  
-        if (data.quantity < 0) {
-
-            // @ts-ignore
-            upload_rollback(req.files['images'] as Express.Multer.File[])
-
-            return res.status(400).json({
-                message: 'Invalid quantity!',
-            });
-        }
-  
-        next();
-      } catch (err) {
-
-        // @ts-ignore
-        upload_rollback(req.files['images'] as Express.Multer.File[])
-
-        return res.status(400).json({
-          message: 'Error while creating product',
-          error: err,
-        });
-      }
-    },
-    
-    async (req: Request, res: Response) => {
-      try {
-        // @ts-ignore
-        if (!req.files || !req.files['images']) {
-
-            // @ts-ignore
-            upload_rollback(req.files['images'] as Express.Multer.File[])
-
-            return res.status(400).json({
-                message: 'Please upload product images',
-            });
-        }
-  
-        // @ts-ignore
-        const images = req.files['images'];
-  
-        const product = new Product({
-          sku: req.body.sku,
-          productName: req.body.productName,
-          description: req.body.description,
-          quantity: req.body.quantity,
-          images: images.map((file: Express.Multer.File) => file.filename),
-          thumbnail: req.body.thumbnail,
-        });
-  
-        await product.save();
-  
-        return res.status(200).json({
-          message: 'New product added',
-          product,
-          images: images.map((file: Express.Multer.File) => file.filename),
-        });
-
-      } catch (err) {
-        // @ts-ignore
-        upload_rollback(req.files['images'] as Express.Multer.File[])
-
-        return res.status(400).json({
-          message: 'Error while creating product',
-          error: err,
-        });
-      }
     }
+  },
+    
+  async (req: Request, res: Response) => {
+    try {
+      // @ts-ignore
+      if (!req.files || !req.files['images']) {
+
+        // @ts-ignore
+        upload_rollback(req.files['images'] as Express.Multer.File[])
+
+        return res.status(400).json({
+          message: 'Please upload product images',
+        });
+      }
+
+      // @ts-ignore
+      const images = req.files['images'];
+
+      const product = new Product({
+        sku: req.body.sku,
+        productName: req.body.productName,
+        description: req.body.description,
+        quantity: req.body.quantity,
+        images: images.map((file: Express.Multer.File) => file.filename),
+        thumbnail: req.body.thumbnail,
+      });
+
+      await product.save();
+
+      return res.status(200).json({
+        message: 'New product added',
+        product,
+        images: images.map((file: Express.Multer.File) => file.filename),
+      });
+
+    } catch (err) {
+      // @ts-ignore
+      upload_rollback(req.files['images'] as Express.Multer.File[])
+
+      return res.status(400).json({
+        message: 'Error while creating product',
+        error: err,
+      });
+    }
+  }
 );
 
-async function upload_rollback(files:Express.Multer.File[]){
-    try{
-        const deletionPromises = files.map((filePath) => deleteFile(filePath.filename));
-        await Promise.all(deletionPromises);
-        console.log('Files deleted successfully');
-    }catch(err){
-        console.error('Error deleting files:', err);
+
+router.get('/get-product' , async (req: Request, res: Response) =>{
+    
+  const id = req.query.id
+
+  if(!id) return res.status(400).json({
+    message : 'Please provide product id'
+  })
+
+  try{
+
+    const product = await Product.findOne({ _id : id })
+
+    if(!product){
+      return res.status(404).json({
+        message : 'product not found',
+        product : null
+      })
     }
+
+    return res.status(200).json({
+      message : 'success',
+      product
+    })
+
+  }catch(err){
+
+    return res.status(500).json({
+      message : 'error while fetching product',
+      product: null
+    })
+
+  }
+
+})
+
+router.get('/get-all-products' , async (req: Request, res: Response) => {
+  // @ts-ignore
+  let page = req.query.page as number || 1;
+  // @ts-ignore
+  let limit = req.query.limit as number || 20;
+  let skip = (page - 1) * limit;
+
+  try{
+    
+    let products = await Product.find()
+      .skip(skip)
+      .limit(limit);
+    
+    return res.status(200).json({
+      products,
+      paging: {
+        count: await Product.countDocuments(),
+        page: page,
+        limit: limit,
+      },
+    });
+
+  }catch(err){
+    return res.status(500).json({
+      message: 'Error while fetching products',
+    });
+  }
+
+})
+
+router.delete('/delete-product' , async (req: Request, res: Response) => {
+
+  const id = req.query.id
+
+  if(!id) return res.status(400).json({
+    message : 'Please provide product id'
+  })
+
+  try{
+
+    const product = await Product.findOne({ _id : id })
+
+    if(!product) return res.status(404).json({
+      message : 'Product not found!'
+    })
+
+
+  }catch(err){
+    return res.status(500).json({
+      message: 'Error while deleting product',
+    });
+  }
+
+})
+
+
+async function upload_rollback(files:Express.Multer.File[]){
+  try{
+    const deletionPromises = files.map((filePath) => deleteFile(filePath.filename));
+    await Promise.all(deletionPromises);
+    console.log('Files deleted successfully');
+  }catch(err){
+    console.error('Error deleting files:', err);
+  }
 
 }
 
